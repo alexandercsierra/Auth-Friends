59413affc7e791910582a5e23899c27aae025a23
Alexander Sierra <alexandercsierra@gmail.com>
2020-02-04 16:27:57 -0500

    changed server for buildweek

--- server.js
+++ server.js
@@ -9,42 +9,7 @@ const token =
 let nextId = 7;
 
 let friends = [
-  {
-    id: 1,
-    name: 'Rachel Green',
-    age: 30,
-    email: 'rachel@friends.com'
-  },
-  {
-    id: 2,
-    name: 'Joey Tribbiani',
-    age: 34,
-    email: 'joey@friends.com'
-  },
-  {
-    id: 3,
-    name: 'Chandler Bing',
-    age: 32,
-    email: 'chandler@friends.com'
-  },
-  {
-    id: 4,
-    name: 'Ross Geller',
-    age: 32,
-    email: 'ross@friends.com'
-  },
-  {
-    id: 5,
-    name: 'Monica Bing',
-    age: 31,
-    email: 'monica@friends.com'
-  },
-  {
-    id: 6,
-    name: 'Phoebe Buffay-Hannigan',
-    age: 30,
-    email: 'phoebe@friends.com'
-  }
+  
 ];
 
 app.use(bodyParser.json());
@@ -90,7 +55,7 @@ app.get('/api/friends/:id', authenticator, (req, res) => {
   }
 });
 
-app.post('/api/friends', authenticator, (req, res) => {
+app.post('/api/friends', (req, res) => {
   const friend = { id: getNextId(), ...req.body };
 
   friends = [...friends, friend];
